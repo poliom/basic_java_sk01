3b0f950174cf8037be5057d4b43f103761ab5a8c
Vidko Videv <vidko33@live.com>
2025-07-24 22:30:36 +0300

    Initial commit

---
+++ src/MyFirstPackage/MyFirstClass.java
@@ -0,0 +1,38 @@
+package MyFirstPackage;
+
+public class MyFirstClass {
+    public static void main(String[] args){
+        System.out.println("Vidko`s first line of code!");
+        int wholeNumber = 100;
+        double fractionalNumber = 132.564;
+        String text = "Vidko loves Yani!";
+        String textKirilica = "Видко обича Яни!";
+
+        System.out.println(wholeNumber);
+        System.out.println(fractionalNumber);
+        System.out.println(text);
+        System.out.println(textKirilica);
+        System.out.println("------------");
+        byte firstByte = 52;
+        long firstLong = 1234L;
+        float firstFloat = 12345.4512F;
+        System.out.println(firstByte);
+        System.out.println(firstLong);
+        System.out.println(firstFloat);
+        System.out.println("------------");
+        boolean firstBoolean = true;
+        boolean secondBoolean = false;
+        System.out.println(firstBoolean);
+        System.out.println(secondBoolean);
+        System.out.println("------------");
+        char singleLetter = 'a';
+        String singleLetterSting = "a";
+        System.out.println(singleLetter);
+        System.out.println(singleLetterSting);
+        System.out.println("------------");
+
+        short firstShort;
+        firstShort = 6547;
+        System.out.println(firstShort);
+    }
+}
---
+++ src/MyFirstPackage/Operators.java
@@ -0,0 +1,20 @@
+package MyFirstPackage;
+
+public class Operators {
+    public static void main(){
+        int numberOne = 100;
+        int numberTwo = 30;
+
+        System.out.println(numberOne + numberTwo);
+        System.out.println(numberOne - numberTwo);
+        System.out.println(numberOne * numberTwo);
+        System.out.println(numberOne / numberTwo);
+        System.out.println(numberOne % numberTwo);
+        System.out.println(++numberOne);
+        System.out.println(--numberTwo);
+
+        double numberDivision = (double) numberOne / numberTwo;
+        System.out.println(numberDivision);
+        System.out.println((double) numberOne / numberTwo);
+    }
+}
